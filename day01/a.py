fd2974200207b11c183259f360c24f6ceb194eee
breakthatbass <gamache.taylor@gmail.com>
2020-12-08 16:28:00 -0500

    refactor day 1

---
+++ day01/a.py
@@ -0,0 +1,25 @@
+import sys
+
+part1 = 0
+part2 = 0
+
+nums = sys.stdin.readlines()
+nums = list(map(int, nums))
+
+# part1
+for num1 in nums:
+    for num2 in  nums:
+        n = num1 + num2
+        if n == 2020:
+            part1 = num1*num2
+
+#part3
+for num1 in nums:
+    for num2 in nums:
+        for num3 in nums:
+            n = num1 + num2 + num3
+            if n == 2020:
+                part2 = num1*num2*num3
+
+print(f'part1: {part1}')
+print(f'part2: {part2}')
--- day01/main.c
+++ day01/main.c
@@ -1,125 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <string.h>
 
-#define MAXDIGIT 10
+#define MAXLINE 10
+#define FILESIZE 300
 
-
-// count_lines: get number of lines from a file
-int count_lines(FILE *fp)
-{
-	char c;
-	int count = 0;
-
-	while ((c = getc(fp)) && c != EOF) {
-		if (c == '\n')
-			count++;
-	}
-	return count;
-}
-
-
-// load_array: add numbers from file into array
-int *load_array(char *file, int file_length)
-{
-	int i;
-	static int *nums;  // array to fill
-	FILE *fp;
-	fp = fopen(file, "r");
-
-	nums = (int*)malloc(sizeof(int) * file_length);
-	if (nums == NULL) {
-		fprintf(stderr, "memory allocation failure...\n");
-		exit(1);
-	}
-
-	char line[MAXDIGIT]; // read into this line
-
-	i = 0;
-	while(fgets(line, MAXDIGIT, fp)) {
-		// convert to int and append to nums array
-		*(nums + i) = atoi(line);
-		i++;
-	}
-	fclose(fp);
-	return nums;
-}
-
-
-// find the first two numbers in arr that add up to 2020
-int part1(int *arr, int len)
+int part1(int *a, int len)
 {
 	int i, j;
 	int mult = 0;
-
-	for (i = 0; i < len; i++) {
-		for ( j = 0; j < len; j++) {
-			if (*(arr+i) + *(arr+j) == 2020) {
-				mult = *(arr+i) * *(arr+j);
-				return mult;
-			}
-		} 
-	}
-	return mult;
-}
-
-
-// find the first THREE numbers in arr that add up to 2020
-int part2(int *arr, int len)
-{
-	int i, j, k;
-	int mult = 0;
+	int passes = 0;
 
 	for (i = 0; i < len; i++) {
 		for (j = 0; j < len; j++) {
-			for (k = 0; k < len; k++) {
-				if (*(arr+i) + *(arr+j) + *(arr+k) == 2020) {
-					mult = *(arr+i) * *(arr+j) * *(arr+k);
-					return mult;
-				}
-			} 
+			passes++;
+			if (*(a+i) + *(a+j) == 2020) {
+				mult = *(a+i) * *(a+j);
+				printf("part1 passes: %d\n", passes);
+				return mult;
+			}
 		}
 	}
 	return mult;
 }
 
 
-
-int main(int argc, char **argv)
+int part2(int *a, int len)
 {
-	FILE *fp;
-	char *file;
+	int i, j, k;
+	int mult = 0;
+	int passes = 0;
 
-	if (argc != 2) {
-		fprintf(stderr, "usage: ./a.out <file>\n");
-		exit(1);
+	for (i = 0; i < len; i++) {
+		for (j = 0; j < len; j++) {
+			for (k = 0; k < len; k++) {
+				passes++;
+				if (*(a+i) + *(a+j) + *(a+k) == 2020) {
+					mult = *(a+i) * *(a+j) * *(a+k);
+					printf("part2 passes: %d\n", passes);
+					return mult;
+				}
+			}
+			
+		}
+	}
+	return mult;
+}
+
+int main()
+{
+	int len = 0;
+	int nums[FILESIZE];
+
+	int i = 0;
+	while (scanf("%d", &nums[i++]) == 1) {
+		len++;
 	}
 
-	file = argv[1];
 
-	fp = fopen(file, "r");
-	if (fp == NULL) {
-		fprintf(stderr, "cannot open file\n");
-		exit(1);
-	}
-
-	int len = count_lines(fp);
-	int *nums = load_array(file, len);
-	int part1_total  = part1(nums, len);
-	int part2_total = part2(nums, len);
-
-	// tests
-	if (strcmp(file, "input_test") == 0) {
-		assert(part1_total == 514579);
-		assert(part2_total == 241861950);
-	}
-
-	printf("The answer is...\n\tpart 1: %d\n\tpart 2: %d\n", part1_total, part2_total);
-	
-	free(nums);
-	fclose(fp);
+	int p1 = part1(nums, len);
+	int p2 = part2(nums, len);
+	printf("part 1: %d\n", p1);
+	printf("part 2: %d\n", p2);
 
 	return 0;
 }
-
