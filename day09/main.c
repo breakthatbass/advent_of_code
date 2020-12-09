9e3cf52baf52bd374ab4b6b173b0fd4b01111b7b
breakthatbass <gamache.taylor@gmail.com>
2020-12-09 09:53:57 -0500

    add part 2

--- day09/main.c
+++ day09/main.c
@@ -21,13 +21,14 @@ void get_pre(int pos)
 int main()
 {
 	int i, j, k;
+	unsigned long invalid;
 
 	i = 0;
 	while (scanf("%lu", &nums[i++]) == 1) {
 		len++;		
 	}
 
-	// PART !	
+	// PART 1	
 	int got_it = 0;
 	for (i = PREAMBLE; i < len; i++) {
 		get_pre(i);
@@ -37,11 +38,48 @@ int main()
 				if ((p[j]+p[k]) == nums[i]) got_it = 1;
 			}
 		}
-		if (got_it == 0) printf("part1: %lu\n", nums[i]);
+		if (got_it == 0) {
+			invalid = nums[i];
+			break;
+		}
 		got_it = 0;
 	}
 
 
+	// PART 2
+	int pos = 0;		// first position of segment when found
+	int passes = 0;		// length of our new array
+	int broke = 0;
+	unsigned long total = 0;
+	for (i = 0; i < len; i++) {
+		for (j = i+1; j < len; j++) {
+			total += nums[j];
+			passes++;
+			if (total == invalid && passes > 1) {
+				pos = i;
+				broke =1;
+				break;
+			} else if (total > invalid) {
+				total = 0;
+				passes = 0;
+				break;
+			}
+		}
+		if (broke == 1) break;
+	}
+	
+	unsigned long low=nums[pos];
+	unsigned long high=0;
+
+	unsigned long ntotal = 0;
+
+	for (i = 0; i < passes; i++) {
+		if (nums[pos+i] > high) high = nums[pos+i];
+		if (nums[pos+i] < low) low = nums[pos+i];
+	}
+
+	printf("part 1: %lu\n", invalid);
+	printf("part 2: %lu\n", low+high);
 
 	return 0;
 }
