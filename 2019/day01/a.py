4de037126faa4a728bb4107ffc7af2d380ec3455
breakthatbass <gamache.taylor@gmail.com>
2020-12-11 05:42:10 -0500

    add part2

--- 2019/day01/a.py
+++ 2019/day01/a.py
@@ -5,17 +5,33 @@ def get_fuel(a):
     i = math.floor(a/3)-2
     return i
 
+#part 2
+def more_fuel(a):
+    mf = []
+    while a > 0:
+        a = get_fuel(a)
+        if a < 0:
+            a = 0
+        mf.append(a)
+    return sum(mf)
+
 
 def main():
-    d = sys.stdin.readlines()
-    d = list(map(int, d))
+    fp = open('input').readlines()
+    fp =list(map(int, fp))
 
     fuel = []
 
-    for n in d:
+    for n in fp:
         fuel.append(get_fuel(n))
     
     total = sum(fuel)
-    print(f'part1 : {total}')
+    print(f'part1: {total}')
+
+    for n in fuel:
+        total+=more_fuel(n)
+
+    print(f'part2: {total}')
+
 main()
 
