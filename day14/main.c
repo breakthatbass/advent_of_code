e802cb9f2b8fa2be2497aed8697e6a3c4eb7f433
breakthatbass <gamache.taylor@gmail.com>
2021-01-12 10:35:02 -0500

    day14 part1!

--- day14/main.c
+++ day14/main.c
@@ -1,84 +1,106 @@
-// not done
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define NBITS 37
-#define MAXLINE 1000
-#define MEMSIZE 70000
+#include "../lib/converters.h" // btod dtob
 
-struct mem {
-    uint64_t loc;
+#define MASK 37
+#define MEMCAP 600
+
+struct memcell {
+    uint64_t addr;
     uint64_t val;
-}; typedef struct mem mem;
+};
+static int size; // keep track of placement
 
-char *convert(uint64_t val)
+// store mem addresses and their masked values
+static struct memcell memory[MEMCAP];
+
+
+char *apply_mask(char *mem, char *mask)
 {
-    static char b[NBITS];
-    int i = 0;
-    while (val > 0) {
-        b[i++] = val % 2;
-        val /= 2;
+    size_t i;
+    static char new_bin[MASK];
+    size_t len = strlen(mask);
+
+    for (i = 0; i < len; i++) {
+        if (mask[i] == 'X') new_bin[i] = mem[i];
+        else if (mask[i] == '0') new_bin[i] = '0';
+        else if (mask[i] == '1') new_bin[i] = '1';
+        else new_bin[i] = mem[i];
     }
-    return b;
+    return new_bin;
 }
 
 
-// load_struct: parse string to get memory location and its value
-mem load_struct(char *line)
+// add a mem addr and mem val to the memcell array
+void insert_mem(uint64_t addr, uint64_t val)
 {
-    mem m;
-    sscanf(line, "mem[%llu] = %llu", &m.loc, &m.val);
-    return m;
-}
-
-void do_mask(char *mask, uint64_t val)
-{
-    char *b = malloc(sizeof(int)*NBITS);
-    b = convert(val);
-    printf("%s\n", b);
     int i;
-    for (i = 0; i < NBITS; i++) {
-        if (mask[i] == '1') {
+    for (i = 0; i < size; i++) 
+        if (memory[i].addr == addr) break;
+    
+    if (i == size) memory[size++].addr = addr;
 
-        }
-    }
+    memory[i].val = val;
 }
+
+uint64_t memcount(void)
+{
+    int i;
+    uint64_t total = 0;
+    for (i = 0; i < size; i++) {
+        printf("addr: %llu  val: %llu\n", memory[i].addr, memory[i].val);
+        total += memory[i].val;
+    }
+    return total;
+}
+
 
 int main()
 {
-    uint64_t *memory = calloc(MEMSIZE, sizeof(uint64_t));
-    char mask[NBITS];
-    char buf[MAXLINE];
-    int total = 0;
+    static char buf[128];
+    char *c;
+    char *msk = malloc(sizeof(char)*MASK);
 
-    int c;
-    int i, p = 0;
-    while ((c = fgetc(stdin)) != EOF) {
-        if (c == '\n') {
-            if (strstr(buf, "mask")) {
-                // move pointer ahead to the value of mask
-                memmove(buf, buf+7, strlen(buf));
-                strcpy(mask, buf);
-            }
-            else if (strstr(buf, "mem")) {
-                mem m = load_struct(buf);
-                memory[m.loc] = m.val;
-                do_mask(mask, m.val);
-            }
-            i = 0;
-            memset(buf, 0, MAXLINE);
-        } else buf[i++] = c;
+    uint64_t total = 0;
+
+    while(fgets(buf, 128, stdin)) {
+        // replace \n with \0
+        c = strchr(buf, '\n');
+        *c = '\0';
+       
+        if (strncmp(buf, "mask", 4) == 0) {
+            strcpy(msk, strchr(buf, '='));
+            msk += 2; // move pointer ahead to bit mask.
+
+
+        } else if (strncmp(buf, "mem", 3) == 0) {
+            int mem_addr;
+            sscanf(buf, "mem[%d", &mem_addr);
+
+            c = strchr(buf, '=');
+            c += 2; // move pointer ahead to numbers
+            
+            
+            char *mem_bin = dtob(atoi(c));
+            char *bin36 = pad_bin(mem_bin);
+            char *masked_mem = apply_mask(bin36, msk);
+
+            insert_mem(mem_addr, btod(masked_mem));
+        }
     }
-
-
-    for (uint64_t j = 0; j < MEMSIZE; j++) {
-        if (memory[j] != 0) 
-        printf("%llu: %llu\n", j, memory[j]);
-    }
-    free(memory);
-
-
+    total = memcount();
+    printf("PART 1: %llu\n", total);
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
