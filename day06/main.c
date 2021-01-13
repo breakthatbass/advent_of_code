08cf28dffff2ea93e544587980f0fb3f7eae2830
breakthatbass <gamache.taylor@gmail.com>
2021-01-13 12:36:45 -0500

    refactor day6 part1

--- day06/main.c
+++ day06/main.c
@@ -1,94 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <advent.h>
+#include <ctype.h>
 
-#define MAXLINE 250
-#define ALPHAMAX 28
-
-
-//void errmsg(char *msg);
-FILE *file_ptr(char *file);
-
-// for use with qsort()
-int cmpfunc (const void * a, const void * b) {return ( *(char*)a - *(char*)b );}
+#define MAXCHAR 28
 
 
-// get answers from group
-int get_answers(char *s)
-{	
-	int i, j;
-	int count = 0;
-	size_t len = strlen(s);
-	int *answer = calloc(len, sizeof(char));
+// store each groups answers
+static char answers[MAXCHAR];
+static int len = 0;
 
-	for (i = 0, j = 0; i < len; i++) {
-		if (s[i] == '\n') continue;
-		if (answer[s[i]] == 1) continue;
-		if (answer[s[i]] == 0) {
-			answer[s[i]] = 1;
-			count++;
-		}
-	}
-	
-	printf("%d\n", count);
-	return count;
+
+// search for c in answers, add if not present
+void find(char c)
+{
+	if (!isalpha(c)) return;
+
+	if (strchr(answers, c) == NULL) 
+		answers[len++] = c;
 }
 
 
 int main(int argc, char **argv)
 {
-	if (argc != 2) errmsg("usage: ./passport <input-file>");
-
-	char *file = argv[1];
-	// file_ptr opens file, error checks, returns ptr
-	FILE *fp = file_ptr(file);
-
-	int c;
-	int i = 0;
+	int c, prev;
 	int total = 0;
-	char buffer[MAXLINE];
-
-
-	while ((c = fgetc(fp))) {
-		// detect a blank line indicating we've got a group
-        if (c == '\n' && buffer[i-1] == '\n') {
-			total += get_answers(buffer);
-			printf("%s\n", buffer);
-			memset(buffer, 0, MAXLINE);
-			i = 0;
-		} else if (feof(fp)) {
-            // we're at the end of file and can't detect '\n\n'
-			total += get_answers(buffer);
-			printf("%s\n", buffer);
+	
+	while ((c = fgetc(stdin))) {
+		if (feof(stdin)) {
+			find(c);
+			total += len;
 			break;
-        } else {
-            buffer[i++] = (char)c;
-        }
+		}
+		if (c == '\n' && prev == '\n') {
+			total += len;
+			len = 0;
+			memset(answers, 0, MAXCHAR);
+		}
+		find(c);
+		prev = c;
 	}
-	printf("total: %d\n", total);
-
+	printf("part 1: %d\n", total);
 	return 0;
-}
-
-
-// HELPERS...
-/*
-// errmsg: print msg and exit
-void errmsg(char *msg)
-{
-    fprintf(stderr, "%s\n", msg);
-    exit(1);
-}
-*/
-
-// file_ptr: open a file ptr, error check, return the ptr
-FILE *file_ptr(char *file)
-{
-    FILE *fp;
-
-    fp = fopen(file, "r");
-    if (fp == NULL) errmsg("cannot open file");
-
-    return fp;
 }
