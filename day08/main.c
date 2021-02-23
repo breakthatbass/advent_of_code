5d51b6443fe730630cb6c7a018b3254c22bfd498
breakthatbass <gamache.taylor@gmail.com>
2021-02-23 13:43:28 -0500

    add part 2

--- day08/main.c
+++ day08/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define INSTRUCT 15
 #define MAXBOOT 2000
@@ -27,13 +28,26 @@ struct bootcode *load_code(char *ins, signed int val)
     return tmp;
 }
 
+
 // run the instructions
-void exec_boot_code(struct bootcode *code)
+// check, 1 if we're looking for last intrsuction
+// check, 0 if we're looking for part 1
+int exec_boot_code(struct bootcode *code, int check)
 {
     int i = 0;
-
     // keep looping until we hit an instruction we've already seen
-    while (code[i].touched == 0) {
+    while (1) {
+
+		if (check && i == len) 
+			// we're on the last instruction, and we've got our part 2
+			return 1;
+		else if ((check && i > len) || code[i].touched) 
+			// we're in an infinite loop
+			return 0;
+		else if (!check && code[i].touched)
+			// part 1: just exit if we've hit a touched instructon
+			return 0;
+
         if (strcmp(code[i].instr, "acc") == 0) {
             code[i].touched = 1;
             accumulator += code[i++].val;
@@ -42,6 +56,57 @@ void exec_boot_code(struct bootcode *code)
             i += code[i].val;
         } else code[i++].touched = 1; // got a nop instruction
     }
+	return 0;
+}
+
+// reset everything to check loop again
+void reset(void)
+{
+	for (int i = 0; i < len; i++)
+		gameboy[i].touched = 0;
+	accumulator = 0;
+}
+
+// change a jmp to nop or a nop to a jmp
+void flip(char *instr)
+{
+	if (strcmp(instr, "jmp") == 0) {
+			memset(instr, 0, INSTRUCT); // clear it
+			strcpy(instr, "nop");
+		}
+	else if(strcmp(instr, "nop") == 0) {
+		memset(instr, 0 , INSTRUCT);
+		strcpy(instr, "jmp");
+	} else return;
+}
+
+// flip an instruction then see if it gets stuck in an infinite loop
+// keep going until we find the right instruction to flip
+void find_corrupted(struct bootcode *code)
+{
+	int i;
+	char lastin[INSTRUCT];
+	signed int lastval;
+
+	// get last spot in intructions
+	strcpy(lastin, code[len-1].instr);
+	lastval = code[len-1].val;
+
+	// loop throuch each instruction, change it
+	// then see if it makes it to the end
+	for (i = 0; i < len; i++) {
+		reset(); // reset touched to 0
+		
+		// change the instruction
+		flip(code[i].instr);
+	
+		// test it out
+		if (exec_boot_code(code, 1) == 1)
+			break;
+
+		flip(code[i].instr); // change back
+
+	}
 }
 
 
@@ -58,9 +123,12 @@ int main()
         free(bc);
         len++;
     }
-   
-    exec_boot_code(gameboy);
-    printf("part 1: %d\n", accumulator);
 
+	exec_boot_code(gameboy, 0);
+    printf("part 1: %d\n", accumulator);
+   
+	find_corrupted(gameboy);
+	printf("part 2: %d\n", accumulator);
+	
     return 0;
 }
