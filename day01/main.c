d37a2259e31695327b350a313753a046560275b6
breakthatbass <gamache.taylor@gmail.com>
2021-06-01 21:13:50 -0400

    add timing and clean up a bit

--- day01/main.c
+++ day01/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <assert.h>
 
+#include "../timing.h"
+
 #define LEN 200
 
 
 int part1(int *n)
 {
-    int t, i, j;
+    int i, j;
     for (i = 0; i < LEN; i++) {
         for (j = 0; j < LEN; j++) {
             if (i == j) continue;
@@ -20,7 +22,7 @@ int part1(int *n)
 
 int part2(int *n)
 {
-    int t, i, j, k;
+    int i, j, k;
     for (i = 0; i < LEN; i++) {
         for (j = 0; j < LEN; j++) {
             for (k = 0; k < LEN; k++) {
@@ -39,16 +41,21 @@ int main()
 
     int nums[LEN];
     int i = 0;
+    timing t;
+
+    start_timing(&t);
     while (fscanf(stdin, "%d", &nums[i++]) == 1);
 
 	int p1 = part1(nums);
 	int p2 = part2(nums);
+    end_timing(&t);
 
 	assert(p1 == 157059);
 	assert(p2 == 165080960);
 	
 	printf("part 1: %d\n", p1);
 	printf("part 2: %d\n", p2);
+    printf("total time: %f\n", t.ttime);  // about 0.011
 
     return 0;
 }
