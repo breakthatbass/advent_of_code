718995e102730133c4a5cc3c81a601c9ee610e0b
breakthatbass <gamache.taylor@gmail.com>
2022-02-24 09:37:45 -0500

    make program faster

--- 2020/day01/main.c
+++ 2020/day01/main.c
@@ -1,38 +1,28 @@
 #include <stdio.h>
 #include <assert.h>
 
-#include "../timing.h"
-
 #define LEN 200
 
+static int part1;
+static int part2;
 
-int part1(int *n)
+// messy but it's efficient
+void count(int *n)
 {
-    int i, j;
-    for (i = 0; i < LEN; i++) {
-        for (j = 0; j < LEN; j++) {
-            if (i == j) continue;
-            if (n[i] + n[j] == 2020) 
-                return n[i]*n[j];
-        }
-    }
-    return 0;
-}
-
-
-int part2(int *n)
-{
-    int i, j, k;
-    for (i = 0; i < LEN; i++) {
-        for (j = 0; j < LEN; j++) {
-            for (k = 0; k < LEN; k++) {
-                if (i == j && i == k) continue;
-                if (n[i] + n[j] + n[k] == 2020)
-                    return n[i]*n[j]*n[k];
-            }
-        }
-    }
-    return 0;
+	for (int i = 0; i < LEN; i++) {
+		for (int j = 0; j < LEN; j++) {
+			if (i == j) continue;
+			if (n[i] + n[j] == 2020) 
+				part1 = n[i] * n[j];
+			for (int k = 0; k < LEN; k++) {
+				if (i == j && i == k) continue;
+				if (n[i] + n[j] + n[k] == 2020) {
+					part2 = n[i] * n[j] * n[k];
+					return;
+				}
+			}
+		}
+	}
 }
 
 
@@ -41,21 +31,12 @@ int main()
 
     int nums[LEN];
     int i = 0;
-    timing t;
-
-    start_timing(&t);
     while (fscanf(stdin, "%d", &nums[i++]) == 1);
 
-	int p1 = part1(nums);
-	int p2 = part2(nums);
-    end_timing(&t);
+	count(nums);
 
-	assert(p1 == 157059);
-	assert(p2 == 165080960);
+	assert(part1 == 157059);
+	assert(part2 == 165080960);
 	
-	printf("part 1: %d\n", p1);
-	printf("part 2: %d\n", p2);
-    printf("total time: %f\n", t.ttime);  // about 0.011
-
     return 0;
 }
