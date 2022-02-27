86c092989565183dfbb92e68a456a85c0f0f0728
breakthatbass <gamache.taylor@gmail.com>
2022-02-26 22:39:40 -0500

    add comment in lib function readints

--- 2019/util.go
+++ 2019/util.go
@@ -6,6 +6,18 @@ import (
 	"strconv"
 )
 
+/**
+ * ReadInts
+ *
+ * @desc: read in lines from stdin and convert to ints
+ *
+ * @return: array of ints
+ *
+ * this functions expects actual ints coming in
+ * as is a typical puzzle input in AoC
+ *
+ * @usage: ./exe < input-file
+ **/
 func ReadInts() []int {
 	var n []int
 	scan := bufio.NewScanner(os.Stdin)
