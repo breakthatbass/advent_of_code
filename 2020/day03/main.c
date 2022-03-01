a5e653a55b575cf57df8a8f7ba32bffc2cc38fe1
breakthatbass <gamache.taylor@gmail.com>
2022-02-28 21:42:48 -0500

    cleanup day 3

--- 2020/day03/main.c
+++ 2020/day03/main.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-#include "../timing.h"
-
 #define MAXLINE 35
 #define MAXLEN 323	// input is 323 lines long
 
@@ -31,9 +29,7 @@ int main()
 	int c;
 	int i = 0;
 	int j = 0;
-	timing t;
 
-	start_timing(&t);
 	while ((c = fgetc(stdin)) != EOF) {
 		if (c == '\n') {
 			i++;
@@ -51,11 +47,9 @@ int main()
 	slopes *= ride_snowy_hill(1, 2);
 
 	long p1 = ride_snowy_hill(3, 1);
-	end_timing(&t);
 
 	printf("part 1: %lu\n", p1);
 	printf("part 2: %lu\n", slopes);
-	printf("total time: %f\n", t.ttime);
 
 	return 0;
 }
