4f39cd9e904f08bae73e8e894b2745fa1cb45795
breakthatbass <gamache.taylor@gmail.com>
2022-03-03 22:39:20 -0500

    make intcode interpreter its own package

--- 2019/day02.go
+++ 2019/day02.go
@@ -17,6 +17,8 @@ import (
 	"strings"
 )
 
+const DEBUG = false
+
 // convert an array of string ints to int ints
 func atoiArray(s []string) []int {
 	var a []int
@@ -40,46 +42,6 @@ func readToString() []string {
 	return intcodes
 }
 
-// run the intcode virtual machine
-func interpreter(intcode []int) int {
-
-	// restore 1202 program alarm
-	intcode[1] = 12
-	intcode[2] = 2
-
-	fmt.Println(intcode)
-
-	for i := 0; i < len(intcode); i += 4 {
-		opcode := intcode[i]
-		input1 := intcode[i+1]
-		input2 := intcode[i+2]
-		output := intcode[i+3]
-
-		fmt.Printf("OP: %d, IN1: %d, IN2: %d, OUT: %d\n", opcode, input1, input2, opcode)
-
-		switch opcode {
-		case 1:
-			// ADD
-			intcode[output] = intcode[input1] + intcode[input2]
-			break
-		case 2:
-			// MULTIPLY
-			intcode[output] = intcode[input1] * intcode[input2]
-			break
-		case 99:
-			// HALT
-			fmt.Println("\n\n")
-			fmt.Println(intcode)
-			return intcode[0]
-			break
-		default:
-			fmt.Printf("error: %d: unknown opcode\n", opcode)
-			break
-		}
-	}
-	return -1
-}
-
 func main() {
 
 	// read in list of ints
@@ -87,6 +49,6 @@ func main() {
 
 	// convert strings to an array of ints
 	intcodes := atoiArray(c)
-	n := interpreter(intcodes)
+	n := Interpreter(intcodes, DEBUG)
 	fmt.Println(n)
 }
---
+++ 2019/intcode.go
@@ -0,0 +1,49 @@
+package main
+
+import "fmt"
+
+// run the intcode virtual machine
+func Interpreter(intcode []int, DEBUG bool) int {
+
+	// restore 1202 program alarm
+	intcode[1] = 12
+	intcode[2] = 2
+
+	if DEBUG {
+		fmt.Println(intcode)
+		fmt.Printf("\nIntCode Program:\n")
+	}
+
+	for i := 0; i < len(intcode); i += 4 {
+		opcode := intcode[i]
+		input1 := intcode[i+1]
+		input2 := intcode[i+2]
+		output := intcode[i+3]
+
+		if DEBUG {
+			fmt.Printf("OP: %d, IN1: %d, IN2: %d, OUT: %d\n", opcode, input1, input2, opcode)
+		}
+
+		switch opcode {
+		case 1:
+			// ADD
+			intcode[output] = intcode[input1] + intcode[input2]
+			break
+		case 2:
+			// MULTIPLY
+			intcode[output] = intcode[input1] * intcode[input2]
+			break
+		case 99:
+			// HALT
+			if DEBUG {
+				fmt.Println()
+				fmt.Println(intcode)
+			}
+			return intcode[0]
+		default:
+			fmt.Printf("error: %d: unknown opcode\n", opcode)
+			break
+		}
+	}
+	return -1
+}
